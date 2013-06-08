a91e388a407de7049ad45c71245bd7f11a83aed2
Alfonso Ayala <ponsho@gmail.com>
2013-06-08 14:57:41 -0500

    Quicksort python

---
+++ qs.py
@@ -0,0 +1,9 @@
+def quicksort(l):
+	if len(l) >= 2:
+		left = [x for x in l[1:] if x < l[0]]
+		right = [x for x in l[1:] if x > l[0]]
+		return quicksort(left) + [l[0]] + quicksort(right)
+	elif len(l) <=1:
+		return l
+
+print quicksort([1,5,6,0,4])
